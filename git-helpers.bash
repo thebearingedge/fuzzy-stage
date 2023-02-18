44c93340146e3bdb6eb4fc79f4f4a911739a495b
Tim Davis <contact@timdav.is>
2023-02-17 20:01:55 -0800

    add a fuzzy diff function

--- git-helpers.bash
+++ git-helpers.bash
@@ -4,9 +4,13 @@
 set -o pipefail
 
 # pipe git status to fzf and git add the result to the index
-
 function fuzzy-stage {
   git status -s | fzf | awk '{print $2}' | xargs git add
+}
+
+# pick a modified file with fzf and view the git diff
+function fuzzy-diff {
+  git status -s | fzf | awk '{print $2}' | xargs git diff
 }
 
 "$@"
